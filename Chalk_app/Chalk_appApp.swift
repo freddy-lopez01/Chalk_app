82dbd5e476aa41c75bda24c86f874f45a3da8e85
Freddy Lopez <freddlopez01@gmail.com>
2023-04-16 15:09:48 -0700

    Initial Commit

---
+++ Chalk_app/Chalk_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chalk_appApp.swift
+//  Chalk_app
+//
+//  Created by Freddy Lopez on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Chalk_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
